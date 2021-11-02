d07802f917ab3737828b02e5ddb81ed46226920c
unknown <dheeraj7121997@gmail.com>
2021-11-02 13:53:43 -0400

    cyk

--- cyk_main.py
+++ cyk_main.py
@@ -18,7 +18,7 @@ def create_cell(first, second):
     return res
 
 
-def read_grammar(filename="./grammar1.txt"):
+def read_grammar(filename="./grammar.txt"):
     """
     reads the rules of a context free grammar from a text file
     :param filename: name of the text file in current directory
@@ -30,8 +30,6 @@ def read_grammar(filename="./grammar1.txt"):
         rules = grammar.readlines()
         v_rules = []
         t_rules = []
-        v = {}
-        t = {}
 
         for rule in rules:
             left, right = rule.split(" -> ")
@@ -43,18 +41,14 @@ def read_grammar(filename="./grammar1.txt"):
                 # it is a terminal
                 if str.islower(ri):
                     t_rules.append([left, ri])
-                    t[ri] = left
 
                 # it is a variable
                 else:
                     v_rules.append([left, ri])
-                    v[ri] = left
-        print(v)  
-        print(t)                 
-        return v, t
+        return v_rules, t_rules
 
 
-def read_input(filename="./input1.txt"):
+def read_input(filename="./input.txt"):
     """
     reads the inputs from a text file
     :param filename: name of the text file in current directory
@@ -67,50 +61,45 @@ def read_input(filename="./input1.txt"):
         for i in inputs:
 
             # remove \n
-            st = i
-    res = st.split()
-    print(res)
+            res.append(i[:-1])
     return res
 
 
-
 def cyk_alg(varies, terms, inp):
+    """
+    implementation of CYK algorithm
+    :param varies: rules related to variables
+    :param terms: rules related to terminals
+    :param inp: input string
+    :return: resulting table
+    """
+
     length = len(inp)
-    Matrix = [[0 for x in range(length)] for y in range(length)] 
-    blocks = []
+    var0 = [va[0] for va in varies]
+    var1 = [va[1] for va in varies]
+
+    # table on which we run the algorithm
+    table = [[set() for _ in range(length-i)] for i in range(length)]
+
+    # Deal with variables
     for i in range(length):
-        Matrix[i][i] = terms[inp[i]] 
-    for i in range(1,length):
-        j=0
-        a= j
-        for k in range(j+i,length):
-                blocks.append([a,k])
-               # Matrix[a][k] = varies[Matrix[a][k]]
-                if(k==length-1):
-                    break
-                a = a + 1
-    print(Matrix) 
-    print(blocks)
-    for each in blocks:
-        i,j = each
-        a = i
-        b = j
-        while Matrix[a][j] == 0:
-            j = j-1
-        while Matrix[i][b] == 0:
-            i = i+1   
-        if Matrix[a][j]+" "+Matrix[i][b] in varies:
-            if not (varies[Matrix[a][j]+" "+Matrix[i][b]] == 'S'):
-                Matrix[a][b] = varies[Matrix[a][j]+" "+Matrix[i][b]]
-            else:
-                print('gg')
-                print (i,j)
-                print(a,b)
-                if(blocks.index(each)==len(blocks)-1):
-                    Matrix[a][b] = varies[Matrix[a][j]+" "+Matrix[i][b]]
-                    
-    print(Matrix) 
-    return inp
+        for te in terms:
+            if inp[i] == te[1]:
+                table[0][i].add(te[0])
+
+    # Deal with terminals
+    # its complexity is O(|G|*n^3)
+    for i in range(1, length):
+        for j in range(length - i):
+            for k in range(i):
+                row = create_cell(table[k][j], table[i-k-1][j+k+1])
+                for ro in row:
+                    if ro in var1:
+                        table[i][j].add(var0[var1.index(ro)])
+
+    # if the last element of table contains "S" the input belongs to the grammar
+    return table
+
 
 def show_result(tab, inp):
     """
@@ -126,14 +115,14 @@ def show_result(tab, inp):
     print()
     for i in range(len(inp)):
         print(i+1, end="")
-        for c in range(len(inp)):
-            if c == 0:
+        for c in tab[i]:
+            if c == set():
                 print("\t{}".format("_"), end="\t")
             else:
                 print("\t{}".format(c), end=" ")
         print()
 
-    if 'S' in tab[0][len(inp)-1]:
+    if 'S' in tab[len(inp)-1][0]:
         print("The input belongs to this context free grammar!")
     else:
         print("The input does not belong to this context free grammar!")
@@ -141,45 +130,6 @@ def show_result(tab, inp):
 
 if __name__ == '__main__':
     v, t = read_grammar()
-    r = read_input()
+    r = read_input()[0]
     ta = cyk_alg(v, t, r)
-    #show_result(ta, r)
-    
-    
-    
-    
-    
-    # def cyk_alg(varies, terms, inp):
-#     """
-#     implementation of CYK algorithm
-#     :param varies: rules related to variables
-#     :param terms: rules related to terminals
-#     :param inp: input string
-#     :return: resulting table
-#     """
-#     print(inp)
-#     length = len(inp)
-#     var0 = [va[0] for va in varies]
-#     var1 = [va[1] for va in varies]
-#     print(var0)
-#     print(var1)
-
-#     # table on which we run the algorithm
-#     table = [[set() for _ in range(length-i)] for i in range(length)]
-#     # Deal with variables
-#     for i in range(length):
-#         for te in terms:
-#             if inp[i] == te[1]:
-#                 table[0][i].add(te[0])          
-#     # Deal with terminals
-#     # its complexity is O(|G|*n^3)
-#     for i in range(1, length):
-#         for j in range(length - i):
-#             for k in range(i):
-#                 row = create_cell(table[k][j], table[i-k-1][j+k+1])
-#                 for ro in row:
-#                     if ro in var1:
-#                         table[i][j].add(var0[var1.index(ro)])
-
-#     # if the last element of table contains "S" the input belongs to the grammar
-#     return table
+    show_result(ta, r)
